9e21d337e930babf40729ebc48bbe7986d45ac0d
joseph <Joseph@example.com>
2023-03-16 00:53:44 -0700

    My first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle")
+#include <stdio.h>
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return 0;
+}
+
