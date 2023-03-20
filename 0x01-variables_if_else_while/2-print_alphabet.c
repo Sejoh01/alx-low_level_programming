060f83e2f14af51e84021a83ab07e3584f60c2f5
joseph <Joseph@example.com>
2023-03-20 13:22:11 -0700

    My first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main- program to print the alphabets
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+char lower;
+
+for (lower = 'a' ; lower <= 'z' ; lower++)
+{
+putchar (lower);
+}
+
+putchar('\n');
+
+return (0);
+}
