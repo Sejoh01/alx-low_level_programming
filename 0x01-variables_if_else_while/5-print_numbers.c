ed9e8c10b6f29ab792bee47fd8b8556c9c96c448
joseph <Joseph@example.com>
2023-03-20 13:34:44 -0700

    Coding so cool

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-prining my numbers
+ *
+ *Return: 0 (Success)
+*/
+
+int main(void)
+{
+int decimal;
+
+for (decimal = 0 ; decimal < 10 ; decimal++)
+{
+printf("%d", decimal);
+}
+putchar('\n');
+return (0);
+}
