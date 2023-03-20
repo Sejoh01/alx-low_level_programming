e6b781911c9e4598221f7bb29dbe8ac3a775d51f
joseph <Joseph@example.com>
2023-03-20 14:01:03 -0700

    Coding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,13 @@
+#include <stdio.h>
 #include <unistd.h>
-#define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+/**
+ * main- programm to print line without certain functions
+ *
+ * Return: 1 (Success)
+ */
 
-int main(void) {
-    write(2, MESSAGE, sizeof(MESSAGE) - 1);
-    return 1;
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
