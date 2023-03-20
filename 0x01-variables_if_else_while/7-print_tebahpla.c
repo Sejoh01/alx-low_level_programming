fbc971f64d2b483fcea27fcc8c4b9acca6ecb495
joseph <Joseph@example.com>
2023-03-20 13:40:09 -0700

    Codong_so_cool

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- reversing my alphabets
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+char reverse;
+
+for (reverse = 'z' ; reverse >= 'a' ; reverse--)
+{
+putchar(reverse);
+}
+putchar('\n');
+return (0);
+}
