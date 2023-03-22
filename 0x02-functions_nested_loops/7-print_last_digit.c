cdc24df04e2a4052fa7514450db156ec26f70b53
joseph <Joseph@example.com>
2023-03-21 23:44:42 -0700

    Coding so cool

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit-print all the last ones
+ *@p:parameter use
+ *Return: the last digits
+*/
+int print_last_digit(int p)
+{
+int check;
+
+check = p % 10;
+if (p < 0)
+{
+check = check * -1;
+}
+_putchar(check + '0');
+return (check);
+
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ *times_table- printing table 9
+ *
+*/
+void times_table(void)
+{
+int i, j, z, u, v;
+
+for (i = 0 ; i <= 9 ; i++)
+{
+for (j = 0 ; j <= 9 ; j++)
+{
+z = i * j;
+if (z > 9)
+{
+u = z % 10;
+v = (z - u) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(v + '0');
+_putchar(u + '0');
+}
+else
+{
+if (j != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
+}
+}
+_putchar('\n');
+}
+
+}
