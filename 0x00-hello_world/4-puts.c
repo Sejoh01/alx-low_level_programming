3a40fa97450bf47dbdf386829446c08e566fca41
Sejoh01 <124854421+Sejoh01@users.noreply.github.com>
2023-05-16 21:26:09 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/**
+/*
  * main-a programm to print a line using function puts
- *
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
